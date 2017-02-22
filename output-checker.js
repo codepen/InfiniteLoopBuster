e005aba23ba12fbe44386d35582c26c2f03e0c0a
Chris Coyier <chriscoyier@gmail.com>
2017-02-22 16:56:16 -0500

    example broken output

--- output-checker.js
+++ output-checker.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 var app = require("./app.js");
 
 app.handler({
-  markup: fs.readFileSync("./example-code/7-in.js", "utf8")
+  markup: fs.readFileSync("./example-code/8-in.js", "utf8")
 }, {
   succeed: function(result) {
     console.log(result.markup);
